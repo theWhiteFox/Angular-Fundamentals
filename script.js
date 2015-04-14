b78562d36db1655d88d5aae0532e287ec3775aa8
Stevo5o <stevo.joc@gmail.com>
2015-04-14 15:15:58 +0100

    Model View Controller Basic

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-// Controllers
+// Controller Capabilities
+// seperation of concerns
+// MVC
 
 (function() {
 
+	// controller
     function MainController($scope) {
 
+    	var person = {
+    		firstName: "SteJ",
+    		lastName: "O'Connor",
+    		imageSrc: "http://stephenoconnor.azurewebsites.net/images/HeadShot.jpg"
+    	};
+
+    	// model
         $scope.message = "Hello, Angular!";
+        $scope.person = person;
 
     };
 
