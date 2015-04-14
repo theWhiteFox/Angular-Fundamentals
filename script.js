53e0f6344b19c71625026f2533178333d189fc83
Stevo5o <stevo.joc@gmail.com>
2015-04-14 13:42:16 +0100

    Functions to build modules

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
-// Code goes here
-// Functions as abstractions
+// Functions to build modules
+// revealing module pattern
 
-var work = function(){
-  
-  console.log("working hard!");
-  
+var createWorker = function(){
+
+	var workCount = 0;
+
+	// encapsulate code inside of function private 
+	var job1 = function(){
+		workCount++;
+		console.log("job1 " + workCount);
+	};
+
+	var job2 = function(){
+		workCount++;
+			console.log("job2 " + workCount);
+	};
+
+	return {
+		job1: job1,
+		job2: job2
+	};
 };
 
-var doWork = function(f){
-  
-  console.log("starting");
-  
-  try {
-    f();
-  }
-  catch(ex){
-    console.log(ex);
-  }
-  console.log("end");
-  
-};
 
-doWork(work);
+var worker = createWorker();
+
+	worker.job1();
+	worker.job2();
+	worker.job2();
+	worker.job2();
+
+
+
