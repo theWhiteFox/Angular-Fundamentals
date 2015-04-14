8a32aa88c8a715d18a618811ab63ec4ef4e43a5b
Stevo5o <stevo.joc@gmail.com>
2015-04-14 13:53:32 +0100

    IFFE to avoid global vars

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
-// Functions to build modules
-// revealing module pattern
+// Function to avoid global variables IIFE
 
-var createWorker = function(){
+// Immediatly invoked function expression (IFFE)
+(function() {
 
-	var workCount = 0;
+	var createWorker = function(){
 
-	// encapsulate code inside of function private 
-	var job1 = function(){
-		workCount++;
-		console.log("job1 " + workCount);
+		var workCount = 0;
+
+		// encapsulate code inside of function private 
+		var job1 = function(){
+			workCount++;
+			console.log("job1 " + workCount);
+		};
+
+		var job2 = function(){
+			workCount++;
+				console.log("job2 " + workCount);
+		};
+
+		return {
+			job1: job1,
+			job2: job2
+		};
 	};
 
-	var job2 = function(){
-		workCount++;
-			console.log("job2 " + workCount);
-	};
 
-	return {
-		job1: job1,
-		job2: job2
-	};
-};
+	var worker = createWorker();
 
+		worker.job1();
+		worker.job2();
+		worker.job2();
+		worker.job2();
 
-var worker = createWorker();
-
-	worker.job1();
-	worker.job2();
-	worker.job2();
-	worker.job2();
-
-
+}()); // end IFFE
 
