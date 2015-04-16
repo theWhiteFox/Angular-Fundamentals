ebd00b7b23a33a6d5a8fa96d574ab4348cb40fa2
Stevo5o <stevo.joc@gmail.com>
2015-04-16 18:37:16 +0100

    UI service

--- script.js
+++ script.js
@@ -3,16 +3,15 @@
 (function() {
 
     // controller
-    function MainController($scope, $http, $interval, $log, $anchorScroll, $location) {
+    function MainController($scope, github, $interval, $log, $anchorScroll, $location) {
 
-        function onUserComplete(response) {
-            $scope.user = response.data;
-            $http.get($scope.user.repos_url)
-                .then(onRepos, onError);
+        function onUserComplete(data) {
+            $scope.user = data;
+            github.getRepos($scope.user).then(onRepos, onError);
         };
 
-        var onRepos = function(response) {
-            $scope.repos = response.data;
+        var onRepos = function(data) {
+            $scope.repos = data;
             $location.hash("userDetails");
             $anchorScroll();
         }
@@ -29,18 +28,18 @@
         };
 
         var countdownInterval = null;
+
         function startCountdown() {
             countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
         };
 
         $scope.search = function(username) {
-        	$log.info("Searching for " + username);
-            $http.get("https://api.github.com/users/" + username)
-                .then(onUserComplete, onError);
-                if(countdownInterval) {
-                	$interval.cancel(countdownInterval);
-                	$scope.countdown = null;
-                }
+            $log.info("Searching for " + username);
+            github.getUser(username).then(onUserComplete, onError);
+            if (countdownInterval) {
+                $interval.cancel(countdownInterval);
+                $scope.countdown = null;
+            }
         };
 
         $scope.username = "angular"
@@ -52,6 +51,6 @@
     };
 
     angular.module('app', [])
-        .controller('MainController', ["$scope", '$http', '$interval','$log','$anchorScroll', '$location', MainController]);
+        .controller('MainController', ["$scope", 'github', '$interval', '$log', '$anchorScroll', '$location', MainController]);
 
 }()); // end IFFE
